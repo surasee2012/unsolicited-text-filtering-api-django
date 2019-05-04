8fbbb186562731fa575f71022a96b7ea9bfeea5f
surasee2012 <doppelganger.2011.g@gmail.com>
2019-05-05 00:45:44 +0700

    Update views.py
    - Rename json 0_prob to prob_0 and 1_prob to prob_1
    - Add Access-Control-Allow-Origin header.

--- the_talk/text_clf_api/views.py
+++ the_talk/text_clf_api/views.py
@@ -4,32 +4,38 @@ from rest_framework import status
 from .serializers import TextSerializer
 from .predictors import predict
 
+
 class Api(APIView):
     def post(self, request):
         serializer = TextSerializer(data=request.data)
 
         if serializer.is_valid():
             text = serializer.data['text']
-            
+
             predict_result = predict(text)
 
             return Response({
                 'agg': {
                     'result': predict_result[0][0],
-                    '0_prob': predict_result[0][1][0],
-                    '1_prob': predict_result[0][1][1],
+                    'prob_0': predict_result[0][1][0],
+                    'prob_1': predict_result[0][1][1],
                 },
                 'obs': {
                     'result': predict_result[1][0],
-                    '0_prob': predict_result[1][1][0],
-                    '1_prob': predict_result[1][1][1],
+                    'prob_0': predict_result[1][1][0],
+                    'prob_1': predict_result[1][1][1],
                 },
                 'spm': {
                     'result': predict_result[2][0],
-                    '0_prob': predict_result[2][1][0],
-                    '1_prob': predict_result[2][1][1],
+                    'prob_0': predict_result[2][1][0],
+                    'prob_1': predict_result[2][1][1],
                 }
-                })
+            }, headers={
+                'Access-Control-Allow-Origin': '*'
+            })
         else:
             return Response(serializer.errors,
-                            status='status.HTTP_40HTTP_400_BAD_REQUEST')
+                            status=status.HTTP_400_BAD_REQUEST,
+                            headers={
+                                'Access-Control-Allow-Origin': '*'
+                            })
